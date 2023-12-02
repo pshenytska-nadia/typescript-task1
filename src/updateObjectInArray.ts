06faaa0b5eaf7fa34440db79a8b62178f7c07924
Nadiia Pshenytska <nadiya.pshenytska@gmail.com>
2023-12-03 01:19:31 +0200

    task2 done

---
+++ src/updateObjectInArray.ts
@@ -0,0 +1,20 @@
+type ObjectShape = {
+  id: number;
+};
+
+export function updateObjectInArray<ObjectShape>(
+  initialArray: ObjectShape[],
+  key: keyof ObjectShape,
+  value: ObjectShape[keyof ObjectShape],
+  patch: Partial<ObjectShape>
+) {
+  const result = initialArray.map((elem) => ({ ...elem }));
+
+  const index = result.findIndex((elem) => elem[key] === value);
+
+  if (index !== -1) {
+    result[index] = { ...result[index], ...patch };
+  }
+
+  return result;
+}
